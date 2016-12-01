f872ed36bd4332161ed49a7c010048d7135f92ed
Shaojung <shaojung@gmail.com>
2016-12-01 10:56:00 +0800

    use Thread to get data

--- app/src/main/java/com/example/user/yvd120101/MainActivity.java
+++ app/src/main/java/com/example/user/yvd120101/MainActivity.java
@@ -16,7 +16,14 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.ref.ReferenceQueue;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -54,6 +61,54 @@ public class MainActivity extends AppCompatActivity {
 
         queue.add(request);
         queue.start();
+    }
 
+    public void click2(View v)
+    {
+        GetData t = new GetData();
+        t.start();
+    }
+
+    private class GetData extends Thread {
+        InputStream inputStream = null;
+        @Override
+        public void run()
+        {
+            URL url = null;
+            try {
+                url = new URL("https://raspberrytemp-abf6c.firebaseio.com/temp.json");
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.connect();
+                inputStream = conn.getInputStream();
+
+                //==============
+                ByteArrayOutputStream result = new ByteArrayOutputStream();
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = inputStream.read(buffer)) != -1) {
+                    result.write(buffer, 0, length);
+                }
+                String str = result.toString();
+                JSONObject obj = new JSONObject(str);
+                final double temp = obj.getDouble("temp");
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tv.setText(String.valueOf(temp));
+                    }
+                });
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
